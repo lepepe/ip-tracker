92ac22e855ec1563e2eba2e890a8524591e17b04
Jose Perez Prol <jose.perez@vertilux.com>
2017-01-22 13:47:55 -0500

    Fixed typo and display more info

--- ip-tracker.rb
+++ ip-tracker.rb
@@ -2,18 +2,18 @@ require 'sinatra'
 require 'geocoder'
 
 get '/' do
-	@location = "Track IP Adress."
-  erb :index
+	@location = "Track IP Address."
+  erb :index, :layout => :main_layout
 end
 
-post '/' do
+post '/tracker' do
 	@location = location(params[:ip_address])
-	erb :index
+	erb :tracker, :layout => :main_layout
 end
 
 def location(ip_address)
-	if location = Geocoder.address(ip_address)
-		return "#{location}"
+	if location = Geocoder.search(ip_address)
+		return location
 	else
 		return "IP Address not found"
 	end
